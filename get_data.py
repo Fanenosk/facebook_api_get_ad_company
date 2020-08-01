07b02337c682b3ce1f36bb030083269074f262b2
Fanenosk <fanenosk@gmail.com>
2020-08-01 13:23:01 +0300

    Last version apps for get info ad company

--- get_data.py
+++ get_data.py
@@ -11,6 +11,7 @@ from facebook_business import adobjects
 import mysql.connector
 from matplotlib import pyplot as plt
 from pandas import DataFrame
+import schedule
 import time
 
 my_access_token = ''
@@ -61,45 +62,48 @@ def get_insights(account, date_preset='last_3d'):
     results = [dict(item) for item in wait_for_async_job(i_async_job)]
     return results
 
-elem_insights = []
-insights_lists = []
-date_preset = 'last_year'
-for elem in my_accounts:
+def in_progress():
+    elem_insights = []
+    insights_lists = []
+    date_preset = 'last_year'
+    for elem in my_accounts:
             elem_insights = get_insights(elem, date_preset)
             insights_lists.append(elem_insights)
 
-insight_campaign_id_list = []
-insight_clicks_list = []
-insight_spend_list = []
-insight_impressions_list = []
-insight_date_start_list = []
-insight_date_stop_list = []
-for elem1 in insights_lists:
-    for elem2 in elem1:
-        insight_campaign_id_list.append(int(elem2['campaign_id']))
-        insight_clicks_list.append(int(elem2['clicks']))
-        insight_spend_list.append(float(elem2['spend']))
-        insight_impressions_list.append(int(elem2['impressions']))
-        insight_date_start_list.append(elem2['date_start'])
-        insight_date_stop_list.append(elem2['date_stop'])
+    insight_campaign_id_list = []
+    insight_clicks_list = []
+    insight_spend_list = []
+    insight_impressions_list = []
+    insight_date_start_list = []
+    insight_date_stop_list = []
+    for elem1 in insights_lists:
+        for elem2 in elem1:
+            insight_campaign_id_list.append(int(elem2['campaign_id']))
+            insight_clicks_list.append(int(elem2['clicks']))
+            insight_spend_list.append(float(elem2['spend']))
+            insight_impressions_list.append(int(elem2['impressions']))
+            insight_date_start_list.append(elem2['date_start'])
+            insight_date_stop_list.append(elem2['date_stop'])
 
-df = DataFrame()
-df['campaign_id'] = insight_campaign_id_list
-df['clicks'] = insight_clicks_list
-df['spend'] = insight_spend_list
-df['impressions'] = insight_impressions_list
-df['date_start'] = insight_date_start_list
-df['date_stop'] = insight_date_stop_list
+    df = DataFrame()
+    df['campaign_id'] = insight_campaign_id_list
+    df['clicks'] = insight_clicks_list
+    df['spend'] = insight_spend_list
+    df['impressions'] = insight_impressions_list
+    df['date_start'] = insight_date_start_list
+    df['date_stop'] = insight_date_stop_list
 
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="root",
-  password="root",
-  database="facebook_insights"
-)
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="root",
+    database="facebook_insights"
+    )
 
-mycursor = mydb.cursor()
+    mycursor = mydb.cursor()
 
-insert_sql = "INSERT into ad_company (campaign_id, clicks, spend, impressions, date_start, date_stop) VALUES (%s, %s, %s, %s, %(to_date)s, %(to_date)s)"
+    insert_sql = "INSERT into ad_company (campaign_id, clicks, spend, impressions, date_start, date_stop) VALUES (%s, %s, %s, %s, %(to_date)s, %(to_date)s)"
 
-mycursor.execute(insert_sql, df)
+    mycursor.execute(insert_sql, df)
+
+schedule.every(30).minutes.do(in_progress())
